b09c0bc67b0a422b059dcbff89d9983067296dfe
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-26 07:47:19 -0300

    feat: backend --> modificado resposta de status no Post de ChamadasPlayList para 201

--- backend/src/main/java/com/andrebarroso/backend/resources/ChamadasPlayListResources.java
+++ backend/src/main/java/com/andrebarroso/backend/resources/ChamadasPlayListResources.java
@@ -1,5 +1,6 @@
 package com.andrebarroso.backend.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andrebarroso.backend.entities.ChamadasPlayList;
 import com.andrebarroso.backend.services.ChamadasPlayListService;
@@ -29,7 +31,10 @@ public class ChamadasPlayListResources {
 	@PostMapping
 	public ResponseEntity<ChamadasPlayList> insert(@RequestBody ChamadasPlayList obj) {
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
