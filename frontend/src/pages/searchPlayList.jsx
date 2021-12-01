caf279f5ceb8233a5bfac11ab8086e0f5de1806f
AndreBarroso <andre_barroso17@hotmail.com>
2021-11-30 23:04:05 -0300

    fix: frontend --> refatora rotina para pegar token

--- frontend/src/pages/searchPlayList.jsx
+++ frontend/src/pages/searchPlayList.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { GetTokenAuthorization } from '../services/resquestAPIs';
-
-const ClientId = '‌4e8d12ac54234d9091767fdf7890e974';
-const ClientSecret = 'f4aa7c2387364f978442b3ed3c46b91e‌';
-const str = ClientId + ':' + ClientSecret;
 
 export default function SearchPlayList() {
 
-  const getToken = async () => {
-
-    const redirectUri = `${window.location.origin}/minhaLista`;
-
-
-    const getCode = () => {
+  const getCode = () => {
     let code = null;
     const queryString = window.location.search;
     if(queryString.length > 0) {
@@ -22,24 +13,21 @@ export default function SearchPlayList() {
     }
   }
 
-  const code = getCode()
+  const getToken = async () => {
 
-        const data = new URLSearchParams();
-        data.append('code', code);
-        data.append('redirect_uri', redirectUri);
-        data.append('grant_type', 'client_credentials');
+    const redirectUri = `${window.location.origin}/minhaLista`;
+    
+    const code = getCode()
 
-        const token = GetTokenAuthorization(data).access_token;
+    const data = new URLSearchParams();
+    data.append('code', code);
+    data.append('redirect_uri', redirectUri);
+    data.append('grant_type', 'client_credentials');
 
-      const result = await fetch('https://accounts.spotify.com/api/token', {
-        headers: {
-            'Content-Type' : 'application/x-www-form-urlencoded', 
-            'Authorization' : 'Basic NGU4ZDEyYWM1NDIzNGQ5MDkxNzY3ZmRmNzg5MGU5NzQ6ZjRhYTdjMjM4NzM2NGY5Nzg0NDJiM2VkM2M0NmI5MWU='
-        },
-        method: 'POST',
-        body: data
-    });
-      console.log('xxxxxxx', await result.json());
+    const token = await GetTokenAuthorization(data);
+    console.log(token)
+
+    return token;
   };
 
   useEffect(() => {
@@ -48,33 +36,7 @@ export default function SearchPlayList() {
 
   return (
     <div>
-      <div>
-          Client ID
-          <input
-           type="text"
-          //  onChange= {(e) => setClientId(e.target.value)}
-           value="4e8d12ac54234d9091767fdf7890e974"
-           />
-        </div>
-        <div>
-          Secret
-          <input
-           type="text"
-          //  onChange= {(e) => setSecret(e.target.value)}
-           value="f4aa7c2387364f978442b3ed3c46b91e"
-           />
-        </div>
-        <button
-          // onClick={handleClick}
-        >
-          envia
-        </button>
+      GETTOKEN
     </div>
   );
 }
-
-// curl -X POST \
-//      -d grant_type=refresh_token \
-//      -d client_id=4e8d12ac54234d9091767fdf7890e974\
-//      -d client_secret=f4aa7c2387364f978442b3ed3c46b91e‌ \
-//      https://accounts.spotify.com/api/token
--- frontend/src/services/resquestAPIs.js
+++ frontend/src/services/resquestAPIs.js
@@ -1,48 +1,18 @@
-// const clientID‌ ‌=  ‌
-// const clientSecret‌ ‌= '‌8c5b88ea1fed465badebafbfa4dfc5de)‌';
-
 const clientId = '4e8d12ac54234d9091767fdf7890e974';
 const clientSecret = '‌f4aa7c2387364f978442b3ed3c46b91e‌';
 const str = clientId + ':' + clientSecret;
-const base64Str = Buffer.from(str, 'utf8').toString('base64');
 
-export const GetTokenAuthorization = async () => {
-    console.log('antes do use route')
-  
-
-    // const route = useRoute();
-    console.log('chegou aqui');
-    // const { code } = route.query;
-    const redirectUri = `${window.location.origin}/handler`;
-
-    console.log(redirectUri, ', ssssssssssss')
-
-    const data = new URLSearchParams();
-    // data.append('code', code);
-    data.append('redirect_uri', redirectUri);
-    data.append('grant_type', 'authorization_code');
-
-    console.log(data, 'meu form')
-
-    // if(code) {
-
-    // }
-
-
-    
+export const GetTokenAuthorization = async (data) => {
     const result = await fetch('https://accounts.spotify.com/api/token', {
-         headers: {
-             'Content-Type' : 'application/x-www-form-urlencoded', 
-             'Authorization' : 'Basic ' + base64Str
-         },
-         method: 'POST',
-         body: data
-     });
+        headers: {
+            'Content-Type' : 'application/x-www-form-urlencoded', 
+            'Authorization' : 'Basic NGU4ZDEyYWM1NDIzNGQ5MDkxNzY3ZmRmNzg5MGU5NzQ6ZjRhYTdjMjM4NzM2NGY5Nzg0NDJiM2VkM2M0NmI5MWU='
+        },
+        method: 'POST',
+        body: data
+    });
 
-     console.log('chegou aqui rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', result);
-
-     
-
-    // const data = await result.json();
-    return data.access_token
+    const resultJson = await result.json();
+    const token = resultJson.access_token;
+    return token;
 }
